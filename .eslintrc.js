a25237337a73075e9927adf657f7d5f813b23345
hconhisway <hatch.on27@gmail.com>
2023-06-30 03:11:01 +0800

    updage eslint check setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    "linebreak-style": ["error", "windows"],
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
